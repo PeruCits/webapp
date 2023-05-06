77e27e1539c9faf678c0ea74c05d4261d9146f8c
Alonso Garrido <55209717+AlonsoGarrido@users.noreply.github.com>
2023-05-06 16:56:49 -0500

    fix:flex

--- perucits/src/components/SearchPlaces.vue
+++ perucits/src/components/SearchPlaces.vue
@@ -1,117 +1,269 @@
 <template>
-  <div id="main" style="margin-top:0">
+  <div id="main" style="margin-top:0;display:flex;flex-direction: column;">
     <h1 style="text-align:center; padding-top:1%;padding-bottom:1%; color:white; font-weight:bolder">Backend</h1>
-    <v-card
-      elevation="24"
-      width="40%"
-      class="mx-auto"
-    >
-      
-      <v-carousel
-        height="400"
-        hide-delimiter-background
-        show-arrows="hover"
-        pause
+    <div style="display:flex; flex-wrap:wrap; align-content: space-around;">
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+        :key="i"
       >
-        <v-carousel-item
-          v-for="(slide, i) in slides"
-          :key="i"
-        >
-        <div style="display:flex" >
-          <v-card-title>{{ slide }}</v-card-title>
-          <v-card-title style="background-color:#61CD4C ;position:absolute; right:0">{{cobertura[i]}}</v-card-title>
-        </div>
         
+          <div style="display:flex" >
+            <v-card-title>DRY: Don't Repeat Yourself</v-card-title>
+            <v-card-title style="background-color:#61CD4C ;position:absolute; right:0">67.66%</v-card-title>
+          </div>
+          
 
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+      >
         
-          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </v-carousel-item>
-      </v-carousel>
-    </v-card>
+          <div style="display:flex" >
+            <v-card-title>No inventar</v-card-title>
+            <v-card-title style="background-color:#4D72F0 ;position:absolute; right:0">100%</v-card-title>
+          </div>
+          
+
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+      >
+        
+          <div style="display:flex" >
+            <v-card-title>Legibilidad antes que optimización</v-card-title>
+            <v-card-title style="background-color:#61CD4C ;position:absolute; right:0">75%</v-card-title>
+          </div>
+          
+
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+      >
+        
+          <div style="display:flex" >
+            <v-card-title>Programar solo código necesario</v-card-title>
+            <v-card-title style="background-color:#F0E44D ;position:absolute; right:0">50%</v-card-title>
+          </div>
+          
+
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+    </div>
     <h1 style="text-align:center; padding-top:1%;padding-bottom:1%; color:white; font-weight:bolder">Frontend</h1>
-    <v-card
-      elevation="24"
-      width="40%"
-      class="mx-auto"
-    >
-      
-      <v-carousel
-        height="400"
-        hide-delimiter-background
-        show-arrows="hover"
-        pause
+    <div style="display:flex; flex-wrap:wrap; align-content: space-around;">
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+        :key="i"
       >
-        <v-carousel-item
-          v-for="(slide, i) in slides"
-          :key="i"
-        >
-        <div style="display:flex" >
-          <v-card-title>{{ slide }}</v-card-title>
-          <v-card-title style="background-color:#61CD4C ;position:absolute; right:0">{{cobertura[i]}}</v-card-title>
-        </div>
         
+          <div style="display:flex" >
+            <v-card-title>Evitar los globales</v-card-title>
+            <v-card-title style="background-color:#4D72F0 ;position:absolute; right:0">100%</v-card-title>
+          </div>
+          
 
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+      >
         
-          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </v-carousel-item>
-      </v-carousel>
-    </v-card>
+          <div style="display:flex" >
+            <v-card-title>Usar [] en vez de New Array()r</v-card-title>
+            <v-card-title style="background-color:#61CD4C ;position:absolute; right:0">67.66%</v-card-title>
+          </div>
+          
+
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+      >
+        
+          <div style="display:flex" >
+            <v-card-title>Usar el operador ternario</v-card-title>
+            <v-card-title style="background-color:#61CD4C ;position:absolute; right:0">66.67%</v-card-title>
+          </div>
+          
+
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+    </div>
     <h1 style="text-align:center; padding-top:1%;padding-bottom:1%; color:white; font-weight:bolder">Testing</h1>
-    <v-card
-      elevation="24"
-      width="40%"
-      class="mx-auto"
-    >
-      
-      <v-carousel
-        height="400"
-        hide-delimiter-background
-        show-arrows="hover"
-        pause
+    <div style="display:flex; flex-wrap:wrap; align-content: space-around;">
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+        :key="i"
       >
-        <v-carousel-item
-          v-for="(slide, i) in slides"
-          :key="i"
-        >
-        <div style="display:flex" >
-          <v-card-title>{{ slide }}</v-card-title>
-          <v-card-title style="background-color:#F0E44D ;position:absolute; right:0">{{cobertura[i]}}</v-card-title>
-        </div>
         
+          <div style="display:flex" >
+            <v-card-title>No duplicar la lógica de implementación</v-card-title>
+            <v-card-title style="background-color:#61CD4C ;position:absolute; right:0">67.66%</v-card-title>
+          </div>
+          
 
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+      >
         
-          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </v-carousel-item>
-      </v-carousel>
-    </v-card>
+          <div style="display:flex" >
+            <v-card-title>Usar it en vez de test</v-card-title>
+            <v-card-title style="background-color:#F0E44D ;position:absolute; right:0">50%</v-card-title>
+          </div>
+          
+
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+      <v-card
+        elevation="24"
+       width="30rem"
+        height="15rem"
+        class="mx-auto"
+      >
+        
+          <div style="display:flex" >
+            <v-card-title>La estructura de los 3 pasos</v-card-title>
+            <v-card-title style="background-color:#F0E44D ;position:absolute; right:0">50%</v-card-title>
+          </div>
+          
+
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+
+
+
+    </div>
     <h1 style="text-align:center; padding-top:1%;padding-bottom:1%; color:white; font-weight:bolder">Seguridad</h1>
-    <v-card
-      elevation="24"
-      width="40%"
-      class="mx-auto"
-    >
-      
-      <v-carousel
-        height="400"
-        hide-delimiter-background
-        show-arrows="hover"
-        pause
+    <div style="display:flex; flex-wrap:wrap; align-content: space-around;">
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+        :key="i"
       >
-        <v-carousel-item
-          v-for="(slide, i) in slides"
-          :key="i"
-        >
-        <div style="display:flex" >
-          <v-card-title>{{ slide }}</v-card-title>
-          <v-card-title style="background-color:#4D72F0 ;position:absolute; right:0">{{cobertura[i]}}</v-card-title>
-        </div>
         
+          <div style="display:flex" >
+            <v-card-title>Cookies seguras</v-card-title>
+            <v-card-title style="background-color:#61CD4C ;position:absolute; right:0">67.66%</v-card-title>
+          </div>
+          
 
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+      >
         
-          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </v-carousel-item>
-      </v-carousel>
-    </v-card>
+          <div style="display:flex" >
+            <v-card-title>Prácticas criptográficas</v-card-title>
+            <v-card-title style="background-color:#61CD4C ;position:absolute; right:0">66.67%</v-card-title>
+          </div>
+          
+
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+      >
+        
+          <div style="display:flex" >
+            <v-card-title>Control de accesos</v-card-title>
+            <v-card-title style="background-color:#61CD4C ;position:absolute; right:0">66.67%</v-card-title>
+          </div>
+          
+
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+      <v-card
+        elevation="24"
+        width="30rem"
+        height="15rem"
+        class="mx-auto"
+      >
+        
+          <div style="display:flex" >
+            <v-card-title>Administración de sesiones</v-card-title>
+            <v-card-title style="background-color:#61CD4C ;position:absolute; right:0">66.67%</v-card-title>
+          </div>
+          
+
+          
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/xt3rY2RQh_0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          
+      </v-card>
+
+
+
+    
+
+
+    </div>
+    
   </div>
 </template>
 
@@ -119,29 +271,6 @@
   export default {
     data () {
       return {
-        colors: [
-          'green',
-          'blue',
-          'green',
-          'yellow',
-        ],
-        cobertura: [
-          '66.67%',
-          '100%',
-          '75%',
-          '50%',
-        ],
-        slides: [
-          "DRY: Don't Repeat Yourself",
-          'No inventar',
-          'Legibilidad antes que optimización',
-          'Programar solo código necesario ',
-        ],
-        slides2: [
-          "Evitar los globales ",
-          'Usar [] en vez de New Array() ',
-          'Usar el operador ternario ',
-        ],
       }
     },
   }
@@ -149,8 +278,7 @@
 
 <style  lang="scss" scoped>
   .v-card{
-    margin: 2%;
-    margin-bottom: 15%;
+    margin-bottom: 10%;
   }
   
 </style>
