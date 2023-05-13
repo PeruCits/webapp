ebcd95e9693b8a2415fff2574de9df10b3a89e33
jaime garrido <u201419735@upc.edu.pe>
2023-05-13 12:36:56 -0500

    ajuste de margenes

--- perucits/src/components/SearchPlaces.vue
+++ perucits/src/components/SearchPlaces.vue
@@ -59,32 +59,33 @@
       </div>
     </section>
     <section class="bg-light page-section" id="services">
-            <div class="container">
-                <div class="text-center" style="margin:5%">
+            <div class="container" style="padding-bottom:5%;">
+                <div class="text-center" style="margin:5%;">
                     <h2 class="section-heading text-uppercase">Etapas del proyecto</h2>
                 </div>
                 <div class="row text-center">
                     <div class="col-md-4">
                         <span class="fa-stack fa-4x">
-                            <span style="font-size:30px;font-weight:bolder;position:relative; top:3%;left:10%; color:white">1</span>
+                            <span style="font-size:30px;font-weight:bolder;position:relative; top:3%;left:7%; color:white">1</span>
                             <svg height="60px" width="60px" class="svg-inline--fa fa-circle fa-stack-2x text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="#111027" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z"></path>2</svg>
                         </span>
                         <h4 class="my-3">Recopilación y análisis de las prácticas</h4>
                         <p class="text-muted">Se analizaron prácticas para cada categoría y realizaron pruebas de concepto para ver cómo GitHub Copilot abordaba dicha práctica.</p>
+                        <br/>
                         <button type="button" class="btn btn-outline-dark" style="font-size:20px;font-weight:bold; padding:10px;margin-top:1%" href="https://github.com/features/copilot">Ver documentación</button>
                     </div>
                     <div class="col-md-4">
                         <span class="fa-stack fa-4x">
-                            <span style="font-size:30px;font-weight:bolder;position:relative; top:3%;left:10%; color:white">2</span>
+                            <span style="font-size:30px;font-weight:bolder;position:relative; top:3%;left:7%; color:white">2</span>
                             <svg height="60px" width="60px" class="svg-inline--fa fa-circle fa-stack-2x text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="#111027" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z"></path>2</svg>
                           </span>
-                        <h4 class="my-3">Diseño del modelo</h4>
+                        <h4 class="my-3">Diseño del modelo de buenas prácticas</h4>
                         <p class="text-muted">Se creó un modelo visual que sirva de apoyo para rápidamente saber qué práctica soporta GitHub Copilot y tener presente en qué porcentaje la herramienta aborda dicha práctica.</p>
                         <button type="button" class="btn btn-outline-dark" style="font-size:20px;font-weight:bold; padding:10px;margin-top:1%" href="https://github.com/features/copilot">Ver modelo</button>
                     </div>
                     <div class="col-md-4">
                         <span class="fa-stack fa-4x">
-                            <span style="font-size:30px;font-weight:bolder;position:relative; top:3%;left:10%; color:white">3</span>
+                            <span style="font-size:30px;font-weight:bolder;position:relative; top:3%;left:7%; color:white">3</span>
                             <svg height="60px" width="60px" class="svg-inline--fa fa-circle fa-stack-2x text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="#111027" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z"></path>2</svg>
                         </span>
                         <h4 class="my-3">Implementación del modelo con una aplicación web</h4>
@@ -95,14 +96,14 @@
             </div>
         </section>
     <section class="py-5"  id="practices">
-      <div class="row gx-5 justify-content-center" style="padding-bottom:3%">
+      <div class="row gx-5 justify-content-center" style="padding-bottom:3%; margin-top: 2%;">
           <div class="text-center my-5">
               <h1  style="text-align:center; padding-top:1%;padding-bottom:2%; color:#111027; font-weight:bolder"><span class="text-gradient d-inline">Prácticas de backend</span></h1>
               <p class="lead fw-light mb-4">Estas prácticas fueron probadas con el lenguaje de programación Java</p>
           </div>
               
       </div>
-      <div style="display:flex; flex-wrap:wrap; align-content: space-around;">
+      <div style="display:flex; flex-wrap:wrap; flex-direction: row; justify-content: flex-start">
       <v-card
         elevation="24"
         width="30rem"
@@ -227,7 +228,7 @@
           <img width="40%" height="100%" src="../../src/components/play (2).png" />
           <div style="display:flex; flex-direction:column;justify-content:center; padding:15px">
           <span style="color:grey; margin-bottom:10px ">Prácticas de frontend</span>
-          <span style="color:black; font-size:25px; font-weight:bold; margin-bottom:10px ">2. Usar [] en vez de New Array()</span>
+          <span style="color:black; font-size:25px; font-weight:bold; margin-bottom:10px ">2. Usar [ ] en vez de New Array( )</span>
           <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">66.67%</div>
           <div class="progress progress-sm mr-2">
             <div class=""  role="progressbar" style="width: 66.67%; background-color:green" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
