43985b3a0c770ac5e66332c87bbbf02164b57193
marciobegazo19 <marciojose19@hotmail.com>
2023-05-20 16:56:52 -0500

    fixfav3

--- perucits/src/components/Favourites.vue
+++ perucits/src/components/Favourites.vue
@@ -1,83 +1,116 @@
 <template>
   <v-container>
+    <v-row class="text-center" style="padding-top: 5%;">
+
     <h2>Favoritos</h2>
     <v-divider></v-divider>
-    <v-row style="padding-top:5%">
-        <div class="d-flex justify-space-around flex-wrap">
-          <v-card
-            class="mx-1 my-4"
-            max-width="344"
-            align="center"
-            v-for="place in places" :key="place.place_id"
-          >
-            
+    <v-row style="padding-top:3%">
+      <div class="d-flex justify-space-around flex-wrap" style="margin: 2%;">
+        <v-card class="mx-1 my-4" max-width="344" align="center" @click="goPlace(place.place_id)" v-for="place in places"
+          :key="place.place_id">
 
-            <div align="center" style="font-weight:bolder; padding:2%; font-size:20px" >
-              {{place.name}}
-            </div>
+          <div align="center" style="font-weight:bolder; padding:2%; font-size:20px">
+            {{ place.name }}
+          </div>
 
-            <div align="center" style=" padding:1%" >
-              {{place.region}}
-            </div>
-            <v-img
-              src="https://cdn.pixabay.com/photo/2016/08/30/18/30/cusco-1631689_960_720.jpg"
-              height="200px"
-              cover
-            ></v-img>
-
-           <svg-icon type="mdi" :path="path" style="margin:2%; color:red" @onclick="likePlace()"></svg-icon>
-
-
-          </v-card>
+          <div align="center" style=" padding:1%">
+            {{ place.region }}
+          </div>
+          <v-img :src="place.image" height="200px"
+            cover></v-img>
           
-        </div>
-      </v-row>
+          <svg-icon type="mdi" :path="path" style="margin:2%; color:red" ></svg-icon>
+          
+
+
+
+
+
+        </v-card>
+
+      </div>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-  import SvgIcon from '@jamescoyle/vue-icon';
-  import { mdiHeart } from '@mdi/js';
-  import axios from 'axios';
-  export default {
-    name: 'FavPlaces',
-    components: {
+import SvgIcon from '@jamescoyle/vue-icon';
+import axios from 'axios';
+import { mdiHeart } from '@mdi/js';
+export default {
+  name: 'FavPlaces',
+  components: {
     SvgIcon
   },
-    data: () => ({
-      selectedRegion: null,
-      selectedLocation: null,
-      selectedPlace: null,
-      path: mdiHeart,
-      places: [],
-    }),
-    methods:{
-      likePlace(){
-        console.log("like");
-      },
-      getFavourites(){
-        //create an axios request to get favourites places from the user
-        axios.get('http://localhost:8080/perucits/favorite?page=0&size=5')
-        .then(response => {
-          this.places = response.data.content;
-        })
-        .catch(error => {
-          console.log(error);
-        })
-      
-      },
+  data: () => ({
+    selectedRegion: null,
+    selectedLocation: null,
+    selectedPlace: null,
+    path: mdiHeart,
+    places: [],
+    regions: ["Costa", "Sierra", "Selva"],
+    locations: ["Norte", "Centro", "Sur"],
+    search: "",
+    favourites: []
+  }),
+  methods: {
+    likePlace(place) {
+      this.favourites.push(place)
+      localStorage.setItem("favourites", JSON.stringify(this.favourites))
     },
-    created(){
-      this.getFavourites()
-    }
+    goPlace(id) {
+      console.log("id es"+id)
+      this.$store.state.placeId = id
+      console.log("store id"+this.$store.state.placeId)
+      this.$router.push("/place")
+    },
+    showPlace(place) {
+      console.log(place)
+
+      this.places = [place]
+      console.log(this.places)
+    },
+    searchPlaces() {
+      console.log(this.search)
+      for (let i = 0; i <= this.places.length; i++) {
+        this.places[i].name == this.search ? this.showPlace(this.places[i]) : console.log("no")
+      }
+    },
+    searchPlaceByRegion() {
+      console.log(this.selectedRegion)
+      for (let i = 0; i <= this.places.length; i++) {
+        this.places[i].region == this.selectedRegion ? this.showPlace(this.places[i]) : console.log("no")
+      }
+    },
+    /*
+    checkFavourite(place_id){
+      for(let i = 0; i < this.favourites.length; i++){
+        if(this.favourites[i].place_id == place_id){
+          return true
+        }
+      }
+    }*/
+  },
+  created() {
+    axios.get('http://localhost:8080/perucits/place?page=0&size=15')
+      .then(response => {
+        console.log(response.data);
+        this.places = response.data.content;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+      this.favourites = JSON.parse(localStorage.getItem("favourites"))
   }
+}
 </script>
 
 <style  lang="scss" scoped>
-  .v-select{
-    padding-right: 5%;
-  }
-  .v-text-field{
-    padding-right: 5%;
-  }
+.v-select {
+  padding-right: 5%;
+}
+
+.v-text-field {
+  padding-right: 5%;
+}
 </style>
