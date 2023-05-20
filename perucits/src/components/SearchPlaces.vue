2bf281debf5a9ef6f0a4b0684755368fe4a06b03
marciobegazo19 <marciojose19@hotmail.com>
2023-05-19 19:36:49 -0500

    rollback2

--- perucits/src/components/SearchPlaces.vue
+++ perucits/src/components/SearchPlaces.vue
@@ -13,7 +13,27 @@
           </div>
       </div>
     </section>
-    <section class="page-section" id="services">
+    <section class="py-5">
+    <div class="row gx-5 justify-content-center" style="padding-bottom:3%">
+          <div class="text-center my-5">
+          <div  style="display:flex; justify-content:center">
+          <h1  style="text-align:center; padding-top:1%;padding-bottom:2%; color:#111027; font-weight:bolder"><span class="text-gradient d-inline">¿Qué es GitHub Copilot?</span></h1>
+          <img
+            alt="Vuetify Logo"
+            src="https://www.podfeet.com/blog/wp-content/uploads/2021/09/GitHub-Copilot-logo-1040x650.png"
+            width="6%"
+            height="6%"
+          />
+          </div>
+
+              
+              <p class="lead fw-light mb-4">Es una IA desarrollada por OpenAI y GitHub para autocompletado de código, utilizando los repositorios de GitHub como base.</p>
+              <button type="button" class="btn btn-outline-dark" style="font-size:20px;font-weight:bold; padding:10px;margin-top:1%" href="https://github.com/features/copilot">Más información</button>
+            </div>
+              
+      </div>
+    </section>
+    <section class="bg-light page-section" id="services">
             <div class="container">
                 <div class="text-center" style="margin:5%">
                     <h2 class="section-heading text-uppercase">Etapas del proyecto</h2>
@@ -21,21 +41,25 @@
                 <div class="row text-center">
                     <div class="col-md-4">
                         <span class="fa-stack fa-4x">
-                            <svg height="60px" width="60px" class="svg-inline--fa fa-circle fa-stack-2x text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z"></path></svg>
+                            <span style="font-size:30px;font-weight:bolder;position:relative; top:3%;left:10%; color:white">1</span>
+                            <svg height="60px" width="60px" class="svg-inline--fa fa-circle fa-stack-2x text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="#111027" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z"></path>2</svg>
                         </span>
                         <h4 class="my-3">Recopilación y análisis de las prácticas</h4>
                         <p class="text-muted">Se analizaron prácticas para cada categoría y realizaron pruebas de concepto para ver cómo GitHub Copilot abordaba dicha práctica.</p>
+                        <button type="button" class="btn btn-outline-dark" style="font-size:20px;font-weight:bold; padding:10px;margin-top:1%" href="https://github.com/features/copilot">Ver documentación</button>
                     </div>
                     <div class="col-md-4">
                         <span class="fa-stack fa-4x">
-                            <svg height="60px" width="60px" class="svg-inline--fa fa-one fa-stack-1x fa-inverse" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="one" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" data-fa-i2svg="desktop"><path fill="#111027" d="M128 32C92.7 32 64 60.7 64 96V352h64V96H512V352h64V96c0-35.3-28.7-64-64-64H128zM19.2 384C8.6 384 0 392.6 0 403.2C0 445.6 34.4 480 76.8 480H563.2c42.4 0 76.8-34.4 76.8-76.8c0-10.6-8.6-19.2-19.2-19.2H19.2z"></path></svg>
+                            <span style="font-size:30px;font-weight:bolder;position:relative; top:3%;left:10%; color:white">2</span>
+                            <svg height="60px" width="60px" class="svg-inline--fa fa-circle fa-stack-2x text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="#111027" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z"></path>2</svg>
                           </span>
-                        <h4 class="my-3">Diseño del modelo de buenas prácticas</h4>
+                        <h4 class="my-3">Diseño del modelo</h4>
                         <p class="text-muted">Se creó un modelo visual que sirva de apoyo para rápidamente saber qué práctica soporta GitHub Copilot y tener presente en qué porcentaje la herramienta aborda dicha práctica.</p>
                     </div>
                     <div class="col-md-4">
                         <span class="fa-stack fa-4x">
-                            <svg height="60px" width="60px" class="svg-inline--fa fa-laptop fa-stack-1x fa-inverse" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="laptop" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" data-fa-i2svg=""><path fill="#111027" d="M128 32C92.7 32 64 60.7 64 96V352h64V96H512V352h64V96c0-35.3-28.7-64-64-64H128zM19.2 384C8.6 384 0 392.6 0 403.2C0 445.6 34.4 480 76.8 480H563.2c42.4 0 76.8-34.4 76.8-76.8c0-10.6-8.6-19.2-19.2-19.2H19.2z"></path></svg>
+                            <span style="font-size:30px;font-weight:bolder;position:relative; top:3%;left:10%; color:white">3</span>
+                            <svg height="60px" width="60px" class="svg-inline--fa fa-circle fa-stack-2x text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="#111027" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512z"></path>2</svg>
                         </span>
                         <h4 class="my-3">Implementación del modelo con una aplicación web</h4>
                         <p class="text-muted">Se puso en práctica toda la información recopilada y el modelo para usar GitHub Copilot de forma correcta y crear una aplicación web que cumpla con buenas prácticas de desarrollo</p>
@@ -43,7 +67,7 @@
                 </div>
             </div>
         </section>
-    <section class="py-5">
+    <section class="py-5"  id="practices">
       <div class="row gx-5 justify-content-center" style="padding-bottom:3%">
           <div class="text-center my-5">
               <h1  style="text-align:center; padding-top:1%;padding-bottom:2%; color:#111027; font-weight:bolder"><span class="text-gradient d-inline">Prácticas de backend</span></h1>
@@ -51,7 +75,7 @@
           </div>
               
       </div>
-      <div style="display:flex; flex-wrap:wrap; flex-direction: row; justify-content: flex-start">
+      <div style="display:flex; flex-wrap:wrap; align-content: space-around;">
       <v-card
         elevation="24"
         width="30rem"
@@ -176,7 +200,7 @@
           <img width="40%" height="100%" src="../../src/components/play (2).png" />
           <div style="display:flex; flex-direction:column;justify-content:center; padding:15px">
           <span style="color:grey; margin-bottom:10px ">Prácticas de frontend</span>
-          <span style="color:black; font-size:25px; font-weight:bold; margin-bottom:10px ">2. Usar [ ] en vez de New Array( )</span>
+          <span style="color:black; font-size:25px; font-weight:bold; margin-bottom:10px ">2. Usar [] en vez de New Array()</span>
           <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">66.67%</div>
           <div class="progress progress-sm mr-2">
             <div class=""  role="progressbar" style="width: 66.67%; background-color:green" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
