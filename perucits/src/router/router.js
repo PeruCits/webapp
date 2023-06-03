ea7c1cc7f0ea649c35e2b3c35498cb808eb2f114
marciobegazo19 <marciojose19@hotmail.com>
2023-06-02 21:42:24 -0500

    fix urls

---
+++ perucits/src/router/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import SearchPlaces from '../components/SearchPlaces.vue'
+import Favourites from '../components/Favourites.vue'
+import Login from '../components/Login.vue'
+import PlaceView from '../components/Place.vue'
+import EditProfile from '../components/EditProfile.vue'
+Vue.use(VueRouter)
+
+const routes = [
+    {path: '/', name: 'SearchPlaces', component: SearchPlaces},
+    {path: '/login', name: 'LoginView', component: Login},
+    {path: '/search', name: 'SearchPlaces', component: SearchPlaces},
+    {path: '/favourites', name: 'FavPlaces', component: Favourites},
+    {path: '/place', name: 'PlaceView', component: PlaceView},
+    {path: '/edit', name: 'EditProfile', component: EditProfile},
+]
+
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
